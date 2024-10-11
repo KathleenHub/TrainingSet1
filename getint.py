592ee4a8c40c13abaeb687353f0a6582016578d7
Kathleen Van Beylen <kathleen.vanbeylen@mycellhub.com>
2024-10-11 14:40:19 +0200

    update11-11

--- getint.py
+++ getint.py
@@ -4,7 +4,7 @@ def get_int(prompt):
             n = int(input(prompt))
             if n>0 and n<9:
                 return n
-        except ValueError:
+        except ValueError: #in case the input is not an integer, ignore and re-prompt for input
             pass
 def main():
     x = get_int("x, a positive integer between 1 and 8: ")
