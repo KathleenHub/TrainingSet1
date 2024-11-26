aa89054d1a34f23dcbc723aa100737fc58bb35ca
Kathleen Van Beylen <kathleen.vanbeylen@mycellhub.com>
2024-11-26 15:19:39 +0100

    update lecture5

--- lecture5/counter.js
+++ lecture5/counter.js
@@ -1,12 +1,20 @@
 
-let counter =0;
+// inspect - application - local storage in webbrowser
+if (!localStorage.getItem('counter')) // ! is not
+    {localStorage.setItem('counter',0);
+}
 function count() {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
-    if (counter % 10 === 0){
+    localStorage.setItem('counter',counter);
+    /*if (counter % 10 === 0){
         alert(`Count is now ${counter}`)
-    }
+    }*/
 }
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = count ; }); 
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+    document.querySelector('button').onclick = count ; 
+    // setInterval(count, 1000); // every one second
+}); 
     
